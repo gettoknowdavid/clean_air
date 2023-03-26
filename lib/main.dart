5f89de891f2bedb884c08664ea8c35167c0a87e4
David Michael II <knowdavidmichael@gmail.com>
2023-03-26 10:26:45 +0100

    config: add firebase config for Firebase Auth, Cloud Firestore, Firebase Storage

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,19 @@ import 'package:clean_air/app/app.bottomsheets.dart';
 import 'package:clean_air/app/app.dart';
 import 'package:clean_air/app/app.dialogs.dart';
 import 'package:clean_air/app/app.locator.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+Future<void> main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   setupLocator();
   setupDialogUi();
   setupBottomSheetUi();

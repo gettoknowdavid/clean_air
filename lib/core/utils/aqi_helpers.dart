d197eff3a296b270f9cf2b5e19f1193bf5178e42
David Michael II <knowdavidmichael@gmail.com>
2023-05-16 17:17:15 +0100

    refactor: add tailored messages for health condition

--- lib/core/utils/aqi_helpers.dart
+++ lib/core/utils/aqi_helpers.dart
@@ -1,3 +1,4 @@
+import 'package:clean_air/models/condition.dart';
 import 'package:clean_air/models/pollution_level.dart';
 import 'package:clean_air/models/weather_type.dart';
 import 'package:clean_air/ui/common/app_colors.dart';
@@ -5,19 +6,28 @@ import 'package:flutter/material.dart';
 
 const String _kGood =
     'Air quality is considered satisfactory, and air pollution poses little or no risk';
-
 const String _kHazardous =
     'Health alert: everyone may experience more serious health effects';
-
 const String _kModerate =
     'Air quality is acceptable; however, for some pollutants there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollution.';
-
 const String _kUnhealthy =
     'Everyone may begin to experience health effects; members of sensitive groups may experience more serious health effects.';
 const String _kUnhealthyForSensitiveGroups =
     'Members of sensitive groups may experience health effects. The general public is not likely to be affected.';
 const String _kVeryUnhealthy =
     'Health warnings of emergency conditions. The entire population is more likely to be affected.';
+
+// Tailored message strings for different health conditions
+const String _kTMAsthma =
+    'Present air quality is dangerous for asthma. Stay indoors and close windows to protect yourself.';
+const String _kTMEmphysema =
+    'Pollutants present in the air may worsen emphysema. Please wear a nose mask to protect yourself from the bad air.';
+const String _kTMBronchitis =
+    'If you have bronchitis, this air is unhealthy for you. Please wear a nose mask and reduce your outdoor exercises to protect yourself from the bad air.';
+const String _kTMHBP =
+    'Present conditions may increase your blood pressure. Please wear a nose mask to protect yourself from the bad air.';
+const String _kTMLungCancer =
+    'May lead to lung cancer. Please wear a nose mask to protect yourself from the bad air.';
 
 Color getColorLegend(int index) {
   switch (index ~/ 50) {
@@ -100,4 +110,31 @@ WeatherType getWeatherType(String name) {
     default:
       return WeatherType.wind;
   }
+}
+
+Map<Condition, Map<String, String>> _getTailoredMessages = {
+  Condition.asthma: {'o3': _kTMAsthma, 'pm25': _kTMAsthma, 'pm10': _kTMAsthma},
+  Condition.hbp: {'o3': _kTMHBP, 'pm25': _kTMHBP, 'pm10': _kTMHBP},
+  Condition.emphysema: {
+    'o3': _kTMEmphysema,
+    'no2': _kTMEmphysema,
+    'pm25': _kTMEmphysema,
+    'pm10': _kTMEmphysema,
+  },
+  Condition.bronchitis: {
+    'no2': _kTMBronchitis,
+    'pm25': _kTMBronchitis,
+    'pm10': _kTMBronchitis,
+  },
+  Condition.lungCancer: {
+    'so2': _kTMLungCancer,
+    'o3': _kTMLungCancer,
+    'no2': _kTMLungCancer,
+    'pm25': _kTMLungCancer,
+    'pm10': _kTMLungCancer,
+  },
+};
+
+String? tailoredMessage(String dominantPol, Condition condition) {
+  return _getTailoredMessages[condition]?[dominantPol];
 }
